faedafde0707164ecec27dc13f79dbd0620c8449
alina-kala4ik <52624465+alina-kala4ik@users.noreply.github.com>
2020-02-27 20:58:50 +0300

    done

---
+++ send.php
@@ -0,0 +1,45 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$text = $_POST['text'];
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $msg = "ok";
+    $mail->isSMTP();
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера GMAIL
+    $mail->Username   = 'pess.by'; // Логин на почте
+    $mail->Password   = '375172058475'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('pess.by@gmail.com', 'pess.by'); // Адрес самой почты и имя отправителя
+    // Получатель письма
+    $mail->addAddress('alina.kala4ik@gmail.com');
+
+        // -----------------------
+        // Само письмо
+        // -----------------------
+        $mail->isHTML(true);
+
+        $mail->Subject = 'pess.by';
+        $mail->Body    = "<b>Имя:</b> $name <br>
+        <b>Почта:</b> $email<br><br>
+        <b>Телефон:</b> $phone<br><br>
+        <b>Сообщение:</b><br>$text";
+// Проверяем отравленность сообщения
+if ($mail->send()) {
+    echo "$msg";
+} else {
+echo "Сообщение не было отправлено. Неверно указаны настройки вашей почты";
+}
+} catch (Exception $e) {
+    echo "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
